597d5521c509fbc9a1befc7d20afdbdccb4df548
Thy Nguyen <nguyenthy2408@gmail.com>
2020-04-29 14:47:08 +0700

    Initial Commit

---
+++ MyMovie-SwiftUITests/MyMovie_SwiftUITests.swift
@@ -0,0 +1,34 @@
+//
+//  MyMovie_SwiftUITests.swift
+//  MyMovie-SwiftUITests
+//
+//  Created by Thy Nguyen on 4/29/20.
+//  Copyright © 2020 Thy Nguyen. All rights reserved.
+//
+
+import XCTest
+@testable import MyMovie_SwiftUI
+
+class MyMovie_SwiftUITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
